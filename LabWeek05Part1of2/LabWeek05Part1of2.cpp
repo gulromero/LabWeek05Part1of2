443697206648ea8e8dfce8e0fbd9de286a5592fd
Gülsüm <gulsumerd38@gmail.com>
2024-11-05 21:54:39 +0800

    opg 1 done ;3

--- LabWeek05Part1of2/LabWeek05Part1of2.cpp
+++ LabWeek05Part1of2/LabWeek05Part1of2.cpp
@@ -3,8 +3,27 @@
 
 #include <iostream>
 
+#define MAX_SIZE 5
+struct Stack {
+    int data[MAX_SIZE];
+    int top;
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    //C program to implement the basic operations of Static Stack data structure.
+    
+    //Declare and initialize an empty stack using Array. 
+
+    Stack* aStack = new Stack;
+
+	for (int k = 0; k <MAX_SIZE; k++) {
+		aStack->data[k] = 0;
+        aStack->top = -1;
+	}
+
+    //Freeing the allocated memory, to avoid memory leaks. 
+    delete aStack;
+    return 0;
 }
 
